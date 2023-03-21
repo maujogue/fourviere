29155afa17549d6e38c693b17de75c264cb470fc
Mathis Aujogue <maujogue@z1r4p4.42lyon.fr>
2023-03-21 15:16:44 +0100

    fixed payment + clean css

--- scripts/partials.js
+++ scripts/partials.js
@@ -4,15 +4,20 @@ document.addEventListener("DOMContentLoaded", function() {
     .then(html => {
       document.querySelector("#head").innerHTML = html;
     });
-  fetch("../partials/header.html")
+    fetch("../partials/header.html")
     .then(response => response.text())
     .then(html => {
       document.querySelector("#header").innerHTML = html;
     });
+    fetch("../partials/header_payment.html")
+      .then(response => response.text())
+      .then(html => {
+        document.querySelector("#header_payment").innerHTML = html;
+      });
   fetch("../partials/footer.html")
     .then(response => response.text())
     .then(html => {
       document.querySelector("#footer").innerHTML = html;
     });
-  
+
 });
